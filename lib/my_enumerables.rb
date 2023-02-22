9db869c7c419d2c493266def09440f32da558f3c
Joel Bejot <joel.bejot@cune.org>
2023-02-22 11:07:09 -0700

    working on it

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,46 @@
 module Enumerable
   # Your code goes here
+  def my_map
+    new_array = []
+    for item in self
+      new_array << yield(item)
+    end
+    new_array
+  end
+
+  def my_each_with_index
+    i = 0
+    for item in self
+      yield item, i
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    array = []
+    for item in self
+      if yield(item) == true
+        array << item
+      end
+    end
+    array
+  end
+
+  def my_all?
+    my_each { |item| return false unless yield item }
+    true
+  end
+
+  def my_any?
+    my_each { |item| return true if yield item }
+    false
+  end
+
+  def my_none?
+    my_each { |item| return false if yield item }
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +49,14 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
+end
+
+
+[1,2,3].my_map do |item|
+  puts item * 2
 end
